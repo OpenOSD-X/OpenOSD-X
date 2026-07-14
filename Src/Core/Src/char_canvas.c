a43ef718d7d2695d6a90b42cb560e7a8e6f00e20
Benedikt Kleiner <benedikt.kleiner@tbs-avionics.com>
2026-07-14 16:00:51 +0800

    Add INAV font paging support

--- Src/Core/Src/char_canvas.c
+++ Src/Core/Src/char_canvas.c
@@ -4,8 +4,8 @@
 #include "main.h"
 #include "char_canvas.h"
 
-static uint8_t canvas_draw[ROW_SIZE_MAX * COLUMN_SIZE_MAX] = {0};
-static uint8_t canvas[ROW_SIZE_MAX * COLUMN_SIZE_MAX] = {0};
+static uint16_t canvas_draw[ROW_SIZE_MAX * COLUMN_SIZE_MAX] = {0};
+static uint16_t canvas[ROW_SIZE_MAX * COLUMN_SIZE_MAX] = {0};
 
 static bool canvas_write = false;
 static bool canvas_next = false;
@@ -27,7 +27,9 @@ void charCanvasInit(void)
 
 void charCanvasClear(void)
 {
-    memset(canvas, ' ', sizeof(canvas));
+    for (int x = 0; x < ROW_SIZE_MAX * COLUMN_SIZE_MAX; x++) {
+        canvas[x] = ' ';
+    }
 }
 
 void charCanvasDraw(void)
@@ -39,10 +41,22 @@ void charCanvasDraw(void)
     };
 }
 
-void charCanvasWrite(uint8_t row, uint8_t column, uint8_t* data, uint8_t len)
+void charCanvasWrite(uint8_t row, uint8_t column, uint8_t attr, uint8_t* data, uint8_t len)
 {
-    if (row < ROW_SIZE_PAL){
-        memcpy(&canvas[row*COLUMN_SIZE + column], data, len);
+    if (row < ROW_SIZE_PAL && column < COLUMN_SIZE){
+        uint8_t font_page = attr & 0x03;
+        uint8_t writable = COLUMN_SIZE - column;
+        uint16_t *dst = &canvas[row*COLUMN_SIZE + column];
+
+        if (len > writable) {
+            len = writable;
+        }
+
+        for (uint8_t x = 0; x < len; x++) {
+            uint16_t ch = ((uint16_t)font_page << 8) | data[x];
+            dst[x] = (ch < FONT_CHARS) ? ch : data[x];
+        }
+
         canvas_write = true;
     }
 }
@@ -54,7 +68,7 @@ void charCanvasNext(void)
     }
 }
 
-uint8_t* charCanvasGet(uint16_t row)
+uint16_t* charCanvasGet(uint16_t row)
 {
     return &canvas_draw[row * COLUMN_SIZE];
 }
